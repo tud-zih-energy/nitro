236eda3948ac9a67702e670b7b354cdad886e721
Mario Bielert <mario.bielert@googlemail.com>
2016-06-23 16:01:26 +0200

    Using env::hostname() in log::attributes::hostname_attribute

--- include/nitro/log/attribute/hostname.hpp
+++ include/nitro/log/attribute/hostname.hpp
@@ -29,50 +29,26 @@
 #ifndef INCLUDE_NITRO_LOG_MESSAGE_HOSTNAME_HPP
 #define INCLUDE_NITRO_LOG_MESSAGE_HOSTNAME_HPP
 
+#include <nitro/env/hostname.hpp>
+
 #include <string>
-
-extern "C" {
-#include <limits.h>
-#include <unistd.h>
-
-// For our beloved MAC
-#ifndef HOST_NAME_MAX
-#ifdef _POSIX_HOST_NAME_MAX
-#define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
-#else
-#define HOST_NAME_MAX 64
-#endif
-#endif
-}
 
 namespace nitro
 {
 namespace log
 {
-
     class hostname_attribute
     {
-        std::string m_hostname;
-
-        static std::string read_hostname()
-        {
-            char c_hostname[HOST_NAME_MAX + 1];
-            if (gethostname(c_hostname, HOST_NAME_MAX + 1))
-            {
-                throw std::runtime_error("Failed to get local hostname");
-            }
-
-            return c_hostname;
-        }
+        std::string hostname_;
 
     public:
-        hostname_attribute() : m_hostname(read_hostname())
+        hostname_attribute() : hostname_(env::hostname())
         {
         }
 
         std::string hostname() const
         {
-            return m_hostname;
+            return hostname_;
         }
     };
 }
