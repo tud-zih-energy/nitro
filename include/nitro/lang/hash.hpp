37c8d4b0bd1374104fb8f9b99a13e9c46e33dad7
Mario Bielert <mario.bielert@googlemail.com>
2017-02-21 18:06:40 +0100

    Adds wrapper for std::unordered_map and std::unordered_set. Burn in hell, std::hash!

---
+++ include/nitro/lang/hash.hpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2015-2017, Technische Universität Dresden, Germany
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ *    endorse or promote products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <functional>
+#include <tuple>
+#include <type_traits>
+
+namespace nitro
+{
+namespace lang
+{
+    // this class is only a tag
+    // in order to use this, implement a hash() member function
+    class hashable
+    {
+    };
+
+    template <typename T>
+    inline typename std::enable_if<!std::is_base_of<hashable, T>::value, std::size_t>::type
+    hash(const T& t);
+
+    template <typename T>
+    inline typename std::enable_if<std::is_base_of<hashable, T>::value, std::size_t>::type
+    hash(const T& t);
+
+    namespace detail
+    {
+        template <typename HashT>
+        inline void hash_combine_impl(HashT& seed, HashT value)
+        {
+            seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        }
+
+        template <std::size_t I, typename T>
+        inline typename std::enable_if<(I == std::tuple_size<T>::value), void>::type
+        hash_combine_tuple(std::size_t&, T const&)
+        {
+        }
+
+        template <std::size_t I, typename T>
+        inline typename std::enable_if<(I < std::tuple_size<T>::value), void>::type
+        hash_combine_tuple(std::size_t& seed, const T& v)
+        {
+            hash_combine_impl(seed, hash(std::get<I>(v)));
+            hash_combine_tuple<I + 1>(seed, v);
+        }
+    }
+
+    template <typename T>
+    inline auto hash_tuple(const T& t)
+    {
+        std::size_t seed = 0;
+        detail::hash_combine_tuple<0>(seed, t);
+        return seed;
+    }
+
+    template <typename T>
+    inline typename std::enable_if<!std::is_base_of<hashable, T>::value, std::size_t>::type
+    hash(const T& t)
+    {
+        return std::hash<T>()(t);
+    }
+
+    template <typename T>
+    inline typename std::enable_if<std::is_base_of<hashable, T>::value, std::size_t>::type
+    hash(const T& t)
+    {
+        return t.hash();
+    }
+
+    template <typename T>
+    struct hash_c
+    {
+        auto operator()(const T& t) const
+        {
+            return hash(t);
+        }
+    };
+}
+}
--- include/nitro/lang/tuple_operators.hpp
+++ include/nitro/lang/tuple_operators.hpp
@@ -1,5 +1,35 @@
+/*
+ * Copyright (c) 2015-2017, Technische Universität Dresden, Germany
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ *    endorse or promote products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef INCLUDE_NITRO_TUPLE_OPERATORS_HPP
 #define INCLUDE_NITRO_TUPLE_OPERATORS_HPP
+
+#include <nitro/lang/hash.hpp>
 
 // included on purpose, though not required in this file for convenience
 #include <tuple>
@@ -85,7 +115,7 @@ namespace lang
     }
 
     template <typename T>
-    struct tuple_operators
+    struct tuple_operators : virtual hashable
     {
         inline friend bool operator!=(const T& x, const T& y)
         {
@@ -110,6 +140,11 @@ namespace lang
         inline friend bool operator>=(const T& x, const T& y)
         {
             return as_tuple(x) >= as_tuple(y);
+        }
+
+        inline auto hash() const
+        {
+            return hash_tuple(as_tuple(static_cast<const T&>(*this)));
         }
 
         template <class Archive>
