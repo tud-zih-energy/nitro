105eb390810b9bfaf640d75a761fde5e60d2a423
Mario Bielert <mario.bielert@tu-dresden.de>
2018-02-14 12:35:40 +0100

    Adds test for opening the binary itself

--- tests/dl_test.cpp
+++ tests/dl_test.cpp
@@ -32,3 +32,30 @@ TEST_CASE("library symbols can be loaded and used", "[dl]")
         REQUIRE_THROWS(test_library.load<double(double)>("nitro_not_defined"));
     }
 }
+
+extern "C" double nitro_binary_cos(double)
+{
+    return 1.0;
+}
+
+TEST_CASE("self binary symbols can be loaded and used", "[dl]")
+{
+    nitro::dl::dl test_library(nitro::dl::self);
+
+    SECTION("nitro_binary_cos is loadable")
+    {
+        REQUIRE_NOTHROW(auto cos = test_library.load<double(double)>("nitro_binary_cos"));
+    }
+
+    SECTION("nitro_binary_cos is callable")
+    {
+        auto cos = test_library.load<double(double)>("nitro_binary_cos");
+
+        REQUIRE(cos(0) == 1.0);
+    }
+
+    SECTION("undefined symbol throws")
+    {
+        REQUIRE_THROWS(test_library.load<double(double)>("nitro_binary_not_defined"));
+    }
+}
