6203f03efc8db26b4bca31a2b0bf8df80416c0ef
Mario Bielert <mario.bielert@tu-dresden.de>
2018-07-26 14:48:51 +0200

    Write out used path in NitroConfig.cmake

--- NitroConfig.cmake
+++ NitroConfig.cmake
@@ -1,1 +1,10 @@
 include("${CMAKE_CURRENT_LIST_DIR}/NitroTargets.cmake")
+
+get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
+get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+
+message(STATUS "Using Nitro: ${_IMPORT_PREFIX}")
+
+set(_IMPORT_PREFIX)
