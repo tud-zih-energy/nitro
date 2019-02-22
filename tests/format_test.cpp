10f8163049db915d8e243d7f2f4c6bc8df6eb39b
Mario Bielert <mario.bielert@tu-dresden.de>
2019-02-22 17:03:07 +0100

    Adds a first iteration of nitro::format

--- tests/format_test.cpp
+++ tests/format_test.cpp
@@ -21,4 +21,66 @@ TEST_CASE("Simple format strings", "[format]")
         REQUIRE(str == nf.str());
         REQUIRE(str == ref_str);
     }
+
+    SECTION("can format with one string argument")
+    {
+        const char* ref_str = "Hello World\n";
+
+        std::string out = nitro::format("{}\n") % "Hello World";
+
+        REQUIRE(out == ref_str);
+    }
+
+    SECTION("can format with multiple string arguments")
+    {
+        const char* ref_str = "Hello World\n";
+
+        std::string out = nitro::format("{} {}\n") % "Hello" % "World";
+
+        REQUIRE(out == ref_str);
+    }
+
+    SECTION("can format with one int arguments")
+    {
+        const char* ref_str = "Hello 8\n";
+
+        std::string out = nitro::format("Hello {}\n") % 8;
+
+        REQUIRE(out == ref_str);
+    }
+
+    SECTION("can format with multiple int arguments")
+    {
+        const char* ref_str = "Hello 8 42\n";
+
+        std::string out = nitro::format("Hello {} {}\n") % 8 % 42;
+
+        REQUIRE(out == ref_str);
+    }
+
+    SECTION("can format a slightly more complex example")
+    {
+        const char* ref_str = "Hello 8 world, the answer is 42!\n";
+
+        std::string out = nitro::format("Hello {} {}, the answer is {}!\n") % 8 % "world" % 42;
+
+        REQUIRE(out == ref_str);
+    }
+}
+
+TEST_CASE("Holding it wrong gives exceptions", "[format]")
+{
+    SECTION("Formatting with more arguments than placeholders throws")
+    {
+        auto fmt = nitro::format("Hello {}\n") % 8 % 42;
+
+        REQUIRE_THROWS(fmt.str());
+    }
+
+    SECTION("Formatting with less arguments than placeholders throws")
+    {
+        auto fmt = nitro::format("Hello {} {}\n") % 8;
+
+        REQUIRE_THROWS(fmt.str());
+    }
 }
