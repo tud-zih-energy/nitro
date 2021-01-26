9356b7d5a9b106f797361d0829a76e351eaf946f
Daddelhai <daddelhaijk@gmail.com>
2021-01-26 13:33:22 +0100

    Added more constexpr

--- tests/fixed_vector_test.cpp
+++ tests/fixed_vector_test.cpp
@@ -5,7 +5,9 @@
 
 TEST_CASE("fixed vector init", "[lang]")
 {
-    nitro::lang::fixed_vector<std::int64_t> v(10);
+    constexpr int SIZE = 10;
+
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE);
 
     REQUIRE(v.size() == 0);
     REQUIRE(v.capacity() == 10);
@@ -13,7 +15,9 @@ TEST_CASE("fixed vector init", "[lang]")
 
 TEST_CASE("fixed vector read and write", "[lang]")
 {
-    nitro::lang::fixed_vector<std::int64_t> v(10);
+    constexpr int SIZE = 10;
+
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE);
 
     v.emplace_back(5);
     v.emplace_back(7);
@@ -27,7 +31,9 @@ TEST_CASE("fixed vector read and write", "[lang]")
 
 TEST_CASE("fixed vector erase", "[lang]")
 {
-    nitro::lang::fixed_vector<std::int64_t> v(10);
+    constexpr int SIZE = 10;
+
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE);
 
     v.emplace_back(5);
     v.emplace_back(7);
@@ -40,9 +46,11 @@ TEST_CASE("fixed vector erase", "[lang]")
 
 TEST_CASE("fixed vector array assignment", "[lang]")
 {
-    constexpr std::array<std::int64_t, 10> a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    
+    constexpr int SIZE = 10;
+    constexpr std::array<std::int64_t, 10> INIT_VALUES = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    nitro::lang::fixed_vector<std::int64_t> v(10, a);
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE, INIT_VALUES);
 
     REQUIRE(v.front() == 0);
     REQUIRE(v.back() == 9);
@@ -51,9 +59,10 @@ TEST_CASE("fixed vector array assignment", "[lang]")
 
 TEST_CASE("fixed vector array assignment with less elements", "[lang]")
 {
-    constexpr std::array<std::int64_t, 6> a = { 0, 1, 2, 3, 4, 5 };
+    constexpr int SIZE = 10;
+    constexpr std::array<std::int64_t, 6> INIT_VALUES = { 0, 1, 2, 3, 4, 5 };
 
-    nitro::lang::fixed_vector<std::int64_t> v(10, a);
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE, INIT_VALUES);
 
     REQUIRE(v.front() == 0);
     REQUIRE(v.back() == 5);
@@ -63,9 +72,10 @@ TEST_CASE("fixed vector array assignment with less elements", "[lang]")
 
 TEST_CASE("fixed vector std::get access", "[lang]")
 {
-    constexpr std::array<std::int64_t, 6> a = { 0, 1, 2, 3, 4, 5 };
+    constexpr int SIZE = 6;
+    constexpr std::array<std::int64_t, 6> INIT_VALUES = { 0, 1, 2, 3, 4, 5 };
 
-    nitro::lang::fixed_vector<std::int64_t> v(6, a);
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE, INIT_VALUES);
 
     REQUIRE(std::get<2>(v) == 2);
     REQUIRE(std::get<3>(v) == 3);
@@ -73,9 +83,10 @@ TEST_CASE("fixed vector std::get access", "[lang]")
 
 TEST_CASE("fixed vector bracket access", "[lang]")
 {
-    constexpr std::array<std::int64_t, 6> a = { 0, 1, 2, 3, 4, 5 };
+    constexpr int SIZE = 10;
+    constexpr std::array<std::int64_t, 6> INIT_VALUES = { 0, 1, 2, 3, 4, 5 };
 
-    nitro::lang::fixed_vector<std::int64_t> v(6, a);
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE, INIT_VALUES);
 
     v[0] = 7;
     v[2] = 9;
@@ -85,9 +96,10 @@ TEST_CASE("fixed vector bracket access", "[lang]")
 
 TEST_CASE("fixed vector at() access", "[lang]")
 {
-    constexpr std::array<std::int64_t, 6> a = { 0, 1, 2, 3, 4, 5 };
+    constexpr int SIZE = 10;
+    constexpr std::array<std::int64_t, 6> INIT_VALUES = { 0, 1, 2, 3, 4, 5 };
 
-    nitro::lang::fixed_vector<std::int64_t> v(6, a);
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE, INIT_VALUES);
 
     v.at(0) = 7;
     v.at(2) = 9;
@@ -95,13 +107,16 @@ TEST_CASE("fixed vector at() access", "[lang]")
     REQUIRE(v.at(2) == 9);
 }
 
-TEST_CASE("fixed vector merge two vectors #1", "[lang]")
+TEST_CASE("fixed vector merge two vectors", "[lang]")
 {
-    constexpr std::array<std::int64_t, 6> a = { 0, 1, 2, 3, 4, 5 };
-    constexpr std::array<std::int64_t, 6> b = { 6, 7, 8, 9, 10, 11 };
+    constexpr int SIZE_DOUBLE = 12;
+    constexpr int SIZE_SINGLE = 6;
 
-    nitro::lang::fixed_vector<std::int64_t> v(12, a);
-    nitro::lang::fixed_vector<std::int64_t> w(6, b);
+    constexpr std::array<std::int64_t, 6> INIT_VALUES_A = { 0, 1, 2, 3, 4, 5 };
+    constexpr std::array<std::int64_t, 6> INIT_VALUES_B = { 6, 7, 8, 9, 10, 11 };
+
+    nitro::lang::fixed_vector<std::int64_t> v(SIZE_DOUBLE, INIT_VALUES_A);
+    nitro::lang::fixed_vector<std::int64_t> w(SIZE_SINGLE, INIT_VALUES_B);
 
     v.push_back(w.begin(), w.end());
 
@@ -117,7 +132,9 @@ TEST_CASE("fixed vector init with init list", "[lang]")
 
 TEST_CASE("fixed vector with unique ptr", "[lang]")
 {
-    nitro::lang::fixed_vector<std::unique_ptr<std::int64_t>> v(2);
+    constexpr int SIZE = 2;
+
+    nitro::lang::fixed_vector<std::unique_ptr<std::int64_t>> v(SIZE);
     v.insert(std::move(std::make_unique<std::int64_t>(5)));
     v.insert(std::move(std::make_unique<std::int64_t>(6)));
 
